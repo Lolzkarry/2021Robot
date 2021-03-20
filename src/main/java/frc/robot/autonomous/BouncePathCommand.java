53e9ea75343b8851b814dd6ca77816a8cdeab58e
Personator11 <tbutler16@claytonschools.net>
2021-03-20 11:22:13 -0500

    Update BouncePathCommand.java

--- src/main/java/frc/robot/autonomous/BouncePathCommand.java
+++ src/main/java/frc/robot/autonomous/BouncePathCommand.java
@@ -14,10 +14,10 @@ public class BouncePathCommand extends SequentialCommandGroup {
     private AdvancedSwerveController controller;
     public BouncePathCommand(OdometricSwerve swerve) {
         addRequirements(swerve);
-        AdvancedSwerveController controllerPart1 = new AdvancedSwerveController(0.1, 0.1, true, 0.1, true, 3, 0, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathCommandPart1").getStates().toArray(Trajectory.State[]::new));
-        AdvancedSwerveController controllerPart2 = new AdvancedSwerveController(0.1, 0.1, true, 0.1, true, 3, 0, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathCommandPart2").getStates().toArray(Trajectory.State[]::new));
-        AdvancedSwerveController controllerPart3 = new AdvancedSwerveController(0.1, 0.1, true, 0.1, true, 3, 0, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathCommandPart3").getStates().toArray(Trajectory.State[]::new));
-        AdvancedSwerveController controllerPart4 = new AdvancedSwerveController(0.1, 0.1, true, 0.1, true, 3, 0, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathCommandPart4").getStates().toArray(Trajectory.State[]::new));
+        AdvancedSwerveController controllerPart1 = new AdvancedSwerveController(0.1, 0.1, true, 0.1, true, 3, 0, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathCommandComponent1").getStates().toArray(Trajectory.State[]::new));
+        AdvancedSwerveController controllerPart2 = new AdvancedSwerveController(0.1, 0.1, true, 0.1, true, 3, 0, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathCommandComponent2").getStates().toArray(Trajectory.State[]::new));
+        AdvancedSwerveController controllerPart3 = new AdvancedSwerveController(0.1, 0.1, true, 0.1, true, 3, 0, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathCommandComponent3").getStates().toArray(Trajectory.State[]::new));
+        AdvancedSwerveController controllerPart4 = new AdvancedSwerveController(0.1, 0.1, true, 0.1, true, 3, 0, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathCommandComponent4").getStates().toArray(Trajectory.State[]::new));
         OdometricSwerve_AdvancedFollowTrajectoryCommand pathPart1 = new OdometricSwerve_AdvancedFollowTrajectoryCommand(swerve, controllerPart1);
         OdometricSwerve_AdvancedFollowTrajectoryCommand pathPart2 = new OdometricSwerve_AdvancedFollowTrajectoryCommand(swerve, controllerPart2);
         OdometricSwerve_AdvancedFollowTrajectoryCommand pathPart3 = new OdometricSwerve_AdvancedFollowTrajectoryCommand(swerve, controllerPart3);
