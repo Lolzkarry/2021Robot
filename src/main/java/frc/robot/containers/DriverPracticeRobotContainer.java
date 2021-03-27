24a6b46379df141daf3a11919951482378cf3086
Vivian Chen <vivianchen@Vivians-MBP.attlocal.net>
2021-03-27 09:10:11 -0500

    git commit -m "am I doing this right?"

--- src/main/java/frc/robot/containers/DriverPracticeRobotContainer.java
+++ src/main/java/frc/robot/containers/DriverPracticeRobotContainer.java
@@ -168,6 +168,11 @@ public class DriverPracticeRobotContainer implements RobotContainer {
             createExampleAutonomousCommand());
 
         SmartDashboard.putData("Selected Auto", autonomousChooser);
+
+        autonomousChooser.addOption(
+            "Barrel Racing",
+            createAutonavBarrelRacingCommand());
+    
     }
 
     private SequentialCommandGroup createTrenchCitrusCompatibleBCommand() {
@@ -548,5 +553,16 @@ public class DriverPracticeRobotContainer implements RobotContainer {
     }
     private void aimAtInnerPort(){
         turretRadianOffset = visionDistanceCalculator.getDesiredTurretOffset(swerve.getCurrentPose().getTranslation(), visionTargetTranslation, innerTargetTranslation);
+    }
+
+    private CommandBase createAutonavBarrelRacingCommand(){
+        return new OdometricSwerve_AdvancedFollowTrajectoryCommand(
+            swerve,
+            createDefaultControllerBuilder()
+            .withEndRotation(new Rotation2d(0.0))
+            .withTrajectory(tryGetDeployedTrajector("BarrelRacing"))
+            .withMaxVelocity(2.0)
+            .buildController());
+        )
     }
 }
