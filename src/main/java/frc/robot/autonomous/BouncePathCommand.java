a39f6279f6c3880198f5a6ad1d2818f4ae58caa8
Personator11 <tbutler16@claytonschools.net>
2021-03-30 15:33:27 -0500

    it work

--- src/main/java/frc/robot/autonomous/BouncePathCommand.java
+++ src/main/java/frc/robot/autonomous/BouncePathCommand.java
@@ -15,28 +15,38 @@ import java.time.Instant;
 import static frc.robot.autonomous.ExtendedTrajectoryUtilities.tryGetDeployedTrajectory;
 
 public class BouncePathCommand extends SequentialCommandGroup {
-    private AdvancedSwerveController controller;
+    private final int samplingInterval;
+    private final double speed;
+    private final double kW;
+
     public BouncePathCommand(OdometricSwerve swerve) {
         addRequirements(swerve);
+        speed = 2;
+        kW = 3;
+        samplingInterval = 1;
+
         InstantCommand resetPose = new InstantCommand(() ->  swerve.resetPose(new Translation2d(0.7752273295375641, 2.349173725871407)));
-        AdvancedSwerveController controllerPart1 = new AdvancedSwerveController(0.1, 0.1, true, 0.15, true, 3, 4, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathComponent1").getStates().toArray(Trajectory.State[]::new));
+        AdvancedSwerveController controllerPart1 = new AdvancedSwerveController(0.1, 0.1, true, 0.15, true, 3, kW, new Rotation2d(),speed,tryGetDeployedTrajectory("BouncePathComponent1").getStates().toArray(Trajectory.State[]::new));
+        AdvancedSwerveController controllerPart2 = new AdvancedSwerveController(0.1, 0.1, true, 0.15, true, 3, kW, new Rotation2d(),speed,tryGetDeployedTrajectory("BouncePathComponent2").getStates().toArray(Trajectory.State[]::new));
+        AdvancedSwerveController controllerPart3 = new AdvancedSwerveController(0.1, 0.1, true, 0.15, true, 3, kW, new Rotation2d(),speed,tryGetDeployedTrajectory("BouncePathComponent3").getStates().toArray(Trajectory.State[]::new));
+        AdvancedSwerveController controllerPart4 = new AdvancedSwerveController(0.1, 0.1, true, 0.15, true, 3, kW, new Rotation2d(),speed,tryGetDeployedTrajectory("BouncePathComponent4").getStates().toArray(Trajectory.State[]::new));
+
         controllerPart1.setContinuousRotation();
-        AdvancedSwerveController controllerPart2 = new AdvancedSwerveController(0.1, 0.1, true, 0.15, true, 3, 4, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathComponent2").getStates().toArray(Trajectory.State[]::new));
         controllerPart2.setContinuousRotation();
-        AdvancedSwerveController controllerPart3 = new AdvancedSwerveController(0.1, 0.1, true, 0.15, true, 3, 4, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathComponent3").getStates().toArray(Trajectory.State[]::new));
         controllerPart3.setContinuousRotation();
-        AdvancedSwerveController controllerPart4 = new AdvancedSwerveController(0.1, 0.1, true, 0.15, true, 3, 4, new Rotation2d(),2.4,tryGetDeployedTrajectory("BouncePathComponent4").getStates().toArray(Trajectory.State[]::new));
         controllerPart4.setContinuousRotation();
         controllerPart1.setSamplingRate(2);
         controllerPart2.setSamplingRate(2);
         controllerPart3.setSamplingRate(2);
         controllerPart4.setSamplingRate(2);
 
-        int samplingRate = 2;
-        controllerPart1.setSamplingRate(samplingRate);
-        controllerPart2.setSamplingRate(samplingRate);
-        controllerPart3.setSamplingRate(samplingRate);
-        controllerPart4.setSamplingRate(samplingRate);
+
+
+        controllerPart1.setSamplingRate(samplingInterval);
+        controllerPart2.setSamplingRate(samplingInterval);
+        controllerPart3.setSamplingRate(samplingInterval);
+        controllerPart4.setSamplingRate(samplingInterval);
+
         OdometricSwerve_AdvancedFollowTrajectoryCommand pathPart1 = new OdometricSwerve_AdvancedFollowTrajectoryCommand(swerve, controllerPart1);
         OdometricSwerve_AdvancedFollowTrajectoryCommand pathPart2 = new OdometricSwerve_AdvancedFollowTrajectoryCommand(swerve, controllerPart2);
         OdometricSwerve_AdvancedFollowTrajectoryCommand pathPart3 = new OdometricSwerve_AdvancedFollowTrajectoryCommand(swerve, controllerPart3);
