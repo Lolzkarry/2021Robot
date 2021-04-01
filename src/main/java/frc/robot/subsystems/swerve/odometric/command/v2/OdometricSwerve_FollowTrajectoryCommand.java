adc18e7d9d52d9fbfcd478c387717bf688863bfc
Octillerysnacker <octillerysnacker@gmail.com>
2021-04-01 14:49:16 -0500

    More odometric shit

--- src/main/java/frc/robot/subsystems/swerve/odometric/command/v2/OdometricSwerve_FollowTrajectoryCommand.java
+++ src/main/java/frc/robot/subsystems/swerve/odometric/command/v2/OdometricSwerve_FollowTrajectoryCommand.java
@@ -8,9 +8,12 @@ import edu.wpi.first.wpilibj.controller.HolonomicDriveController;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.trajectory.Trajectory.State;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
@@ -18,23 +21,34 @@ import frc.robot.subsystems.swerve.odometric.OdometricSwerve;
 
 public class OdometricSwerve_FollowTrajectoryCommand extends CommandBase {
 
-  private OdometricSwerve swerve;
-  private Trajectory trajectory;
-  private Timer timer;
-  private HolonomicDriveController controller;
+  protected OdometricSwerve swerve;
+  protected Trajectory trajectory;
+  protected Timer timer = new Timer();
+  protected HolonomicDriveController controller;
+  protected double[] currentState = new double[]{0,0};
+  protected Translation2d currentTranslation;
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     timer.reset();
     timer.start();
+    currentTranslation = trajectory.getInitialPose().getTranslation();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    var state = controller.calculate(swerve.getCurrentPose(), trajectory.sample(timer.get()), new Rotation2d());
-    swerve.moveFieldCentric(state.vxMetersPerSecond, state.vyMetersPerSecond, state.omegaRadiansPerSecond);
+    var state = trajectory.sample(timer.get());
+    applyState(state);
+  }
+
+  protected void applyState(State state) {
+    currentTranslation = state.poseMeters.getTranslation();
+    currentState[0] = state.poseMeters.getX();
+    currentState[1] = state.poseMeters.getY();
+    var output = controller.calculate(swerve.getCurrentPose(), state, new Rotation2d());
+    swerve.moveFieldCentric(output.vxMetersPerSecond, output.vyMetersPerSecond, output.omegaRadiansPerSecond);
   }
 
   // Called once the command ends or is interrupted.
@@ -74,5 +88,11 @@ public class OdometricSwerve_FollowTrajectoryCommand extends CommandBase {
   }
   public static HolonomicDriveController createBasicController(double kPx, double kPy, double kPw, double maxRotationalSpeed, double maxRotationlAcceleration){
     return new HolonomicDriveController(new PIDController(kPx, 0, 0), new PIDController(kPy, 0, 0), new ProfiledPIDController(kPw, 0, 0, new Constraints(maxRotationalSpeed, maxRotationlAcceleration) ));
+  }
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    super.initSendable(builder);
+    builder.addDoubleArrayProperty("Current State", () -> currentState, null);
+    builder.addDoubleProperty("Trajectory Time", () -> trajectory.getTotalTimeSeconds(), null);
   }
 }
