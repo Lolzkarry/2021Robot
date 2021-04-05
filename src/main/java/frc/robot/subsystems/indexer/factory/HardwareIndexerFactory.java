84a4f09a72b4b04a7082bd6ed12dd18b480d72a0
Octillerysnacker <octillerysnacker@gmail.com>
2021-04-04 21:10:06 -0500

    Added AnalongPotentionmeter to hardware indexer

--- src/main/java/frc/robot/subsystems/indexer/factory/HardwareIndexerFactory.java
+++ src/main/java/frc/robot/subsystems/indexer/factory/HardwareIndexerFactory.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.indexer.factory;
 
 import frc.robot.components.hardware.TalonSRXComponent;
+import frc.robot.subsystems.indexer.AnalogPotentiometerBallSensor;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.indexer.NetworkTableBallSensor;
 
@@ -9,6 +10,8 @@ public class HardwareIndexerFactory implements IndexerFactory {
      *
      */
     private static final int BALL_SENSOR_THRESHOLD = 40;
+    private static final int CHANNEL = 1;
+    private static final double FULL_RANGE = 10, OFFSET = 0, MAIN_THRESHOLD = 10;
     /**
      *
      */
@@ -16,7 +19,7 @@ public class HardwareIndexerFactory implements IndexerFactory {
     public Indexer makeIndexer(){
         return new Indexer(
             new TalonSRXComponent(INDEXER_MOTOR_PORT), 
-            new NetworkTableBallSensor("Sensor0", BALL_SENSOR_THRESHOLD+5), 
+            new AnalogPotentiometerBallSensor(CHANNEL, FULL_RANGE, OFFSET, MAIN_THRESHOLD), 
             new NetworkTableBallSensor("Sensor1", BALL_SENSOR_THRESHOLD+15), 
             new NetworkTableBallSensor("Sensor2", BALL_SENSOR_THRESHOLD), 
             new NetworkTableBallSensor("Sensor3", BALL_SENSOR_THRESHOLD), 
