61d7149154a30eeecd1fa71ada295ac43283b62f
Octillerysnacker <octillerysnacker@gmail.com>
2021-04-04 21:14:35 -0500

    Added Virtual Ball Sensor

---
+++ src/main/java/frc/robot/subsystems/indexer/VirtualBallSensor.java
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.indexer;
+
+/** Add your docs here. */
+public class VirtualBallSensor implements BallSensor{
+
+    @Override
+    public boolean registersBall() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
